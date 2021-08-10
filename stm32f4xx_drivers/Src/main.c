33078d048a5623f9c1cca77b465469545e4f18d5
Eloy AM <eloy.abellanm@um.es>
2021-08-10 21:23:47 +0200

    SPI testing

--- stm32f4xx_drivers/Src/main.c
+++ stm32f4xx_drivers/Src/main.c
@@ -23,14 +23,37 @@
 
 #include "stm32f429xx.h"
 #include "stm32f429_gpio_driver.h"
+#include "stm32f429xx_spi_driver.h"
 
 void delay(void)
 {
 	for (uint32_t i = 0; i < 500000; ++i);
 }
 
+void SPI2_GPIOInits(void);
+void SPI2_Inits(void);
+
+
 int main(void)
 {
+
+	/*
+	 * SPI pins
+	 *
+	 * PB14 SPI2_MISO
+	 * PB15 SPI2_MOSI
+	 * PB13 SPI2_SCLK
+	 * PB12 SPI2_NSS
+	 *
+	 * Alt func mode: 5
+	 */
+	SPI2_GPIOInits();
+	SPI2_Inits();
+	SPI_PeripheralControl(SPI2, ENABLE);
+	SPI_SendData(SPI2, (uint8_t*)"hello", 5);
+	while (1) {}
+
+	/*
 	GPIO_Handle_t GpioLed;
 
 	GpioLed.pGPIOx = GPIOD;
@@ -48,10 +71,56 @@ int main(void)
 		GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
 		delay();
 	}
+	*/
 
 	return 0;
 
 
     /* Loop forever */
 //	for(;;);
+}
+
+void SPI2_GPIOInits(void)
+{
+	GPIO_Handle_t SPIPins;
+
+	SPIPins.pGPIOx = GPIOB;
+	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode = 5;
+	SPIPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	SPIPins.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+
+	// SCLK
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIO_Init(&SPIPins);
+
+	// MOSI
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_15;
+	GPIO_Init(&SPIPins);
+
+	// MISO
+//	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_14;
+//	GPIO_Init(&SPIPins);
+
+	// NSS
+//	SPIPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+//	GPIO_Init(&SPIPins);
+}
+
+void SPI2_Inits(void)
+{
+	SPI_Handle_t SPI2handle;
+
+	SPI2handle.pSPIx = SPI2;
+
+	SPI2handle.SPIConfig.SPI_BusConfig = SPI_BUS_CONFIG_FD;
+	SPI2handle.SPIConfig.SPI_DeviceMode = SPI_DEVICE_MODE_MASTER;
+	SPI2handle.SPIConfig.SPI_SclkSpeed = SPI_SCLK_SPEED_DIV2;
+	SPI2handle.SPIConfig.SPI_DFF = SPI_DFF_8BITS;
+	SPI2handle.SPIConfig.SPI_CPOL = SPI_CPOL_LOW;
+	SPI2handle.SPIConfig.SPI_CPHA = SPI_CPOL_LOW;
+	SPI2handle.SPIConfig.SPI_SSM = SPI_SSM_EN; // software slave management
+
+	SPI_Init(&SPI2handle);
 }
