65a4a11010105c47cb44016c56213963f9bb7faf
Eloy AM <eloy.abellanm@um.es>
2021-08-14 19:10:39 +0200

    RCC_GetPCLK2Value

--- stm32f4xx_drivers/drivers/Src/stm32f29xx_rcc_driver.c
+++ stm32f4xx_drivers/drivers/Src/stm32f29xx_rcc_driver.c
@@ -40,5 +40,38 @@ uint32_t RCC_GetPCLK1Value(void)
 	return pckl1;
 }
 
-// TODO Use as reference RCC_GetPCLK1Value(void)
-uint32_t RCC_GetPCLK2Value(void);
+
+uint32_t RCC_GetPCLK2Value(void)
+{
+	uint32_t pclk2, SystemClk = 0, temp;
+
+	uint8_t clksrc = ((RCC->CFGR >> 2) & 0x3);
+
+	uint8_t ahbp, apb2p;
+
+	switch (clksrc) {
+		case 0: SystemClk = 16000000; break;
+		case 1: SystemClk =  8000000; break;
+		case 2: SystemClk = RCC_GetPLLOutputClock(); break;
+	}
+
+	/* AHBP */
+	temp = ((RCC->CFGR >> 4) & 0xF);
+	if (temp < 8) {
+		ahbp = 1;
+	} else {
+		ahbp = AHB_PreScaler[temp-8];
+	}
+
+	/* APB2 */
+	temp = ((RCC->CFGR >> 13) & 0x7);
+	if (temp < 4) {
+		apb2p = 1;
+	} else {
+		apb2p = APB_PreScaler[temp-4];
+	}
+
+	pclk2 = (SystemClk / ahbp) / apb2p;
+
+	return pclk2;
+}
