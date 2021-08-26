ca14c33cbf7b8392fcbf4bac4b77a9318fabd5df
Eloy AM <eloy.abellanm@um.es>
2021-08-26 10:07:31 +0200

    LCD Init

---
+++ stm32f4xx_drivers/bsp/lcd.c
@@ -0,0 +1,74 @@
+#include "lcd.h"
+
+static void write_4_bits(uint8_t value);
+static void mdelay(uint32_t milis);
+static void udelay(uint32_t micros);
+static void lcd_enable(void);
+
+void lcd_init(void)
+{
+	// 1. Configure the gpio pins wich are used for LCD connections
+	GPIO_Handle_t lcd_signal;
+
+	lcd_signal.pGPIOx = LCD_GPIO_PORT;
+	lcd_signal.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_RS;
+	lcd_signal.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	lcd_signal.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	lcd_signal.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_RW;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_EN;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_D4;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_D5;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_D6;
+	GPIO_Init(&lcd_signal);
+
+	lcd_signal.GPIO_PinConfig.GPIO_PinNumber = LCD_GPIO_D7;
+	GPIO_Init(&lcd_signal);
+
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_EN, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D4, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D5, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D6, GPIO_PIN_RESET);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, GPIO_PIN_RESET);
+
+	// 2. Do the LCD initialization
+//	mdelay(40);
+
+	/* RS = 0, For LCD command */
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RS, GPIO_PIN_RESET);
+
+	/* RnW = 0, Writing to LCD */
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_RW, GPIO_PIN_RESET);
+
+	write_4_bits(0x3); // 0011 = 3
+
+	mdelay(5);
+
+	write_4_bits(0x3); // 0011 = 3
+
+	udelay(150);
+}
+
+
+static void write_4_bits(uint8_t value)
+{
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D4, (value >> 0) & 0x1);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D5, (value >> 1) & 0x1);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D6, (value >> 2) & 0x1);
+	GPIO_WriteToOutputPin(LCD_GPIO_PORT, LCD_GPIO_D7, (value >> 3) & 0x1);
+
+	lcd_enable();
+}
